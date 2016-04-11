78ba708e690eb6d408ce3cbac58594d63225dc57
Sebastian @ Travis CI <sebastian@zillessen.info>
2016-04-11 18:27:47 +0100

    Automatically Deploy to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ python build.py build
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
 git commit -am "Automatically Deploy to GitHub Pages"
-git status
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" `git subtree split --prefix site master`:gh-pages > /dev/null 2>&1
+git push "https://${GH_TOKEN}:x-oauth-basic@github.com/sebastianzillessen/datenwirken.de.git" master
+git push --force --quiet "https://${GH_TOKEN}:x-oauth-basic@github.com/sebastianzillessen/datenwirken.de.git" `git subtree split --prefix site master`:gh-pages > /dev/null 2>&1
