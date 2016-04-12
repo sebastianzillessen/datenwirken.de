6a3ffa166c332973eb10c32836f5048de83152c5
Sebastian @ Travis CI <sebastian@zillessen.info>
2016-04-12 13:54:39 +0100

    removed token

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,10 @@ git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 git add .
 git commit --amend --no-edit
-TOK=""
 echo "We are on $TRAVIS_REPO_SLUG"
-echo "TEST: $TEST"
-if [ "$TRAVIS_REPO_SLUG" == "sebastianzillessen/datenwirken.de" ]; then
-    TOK=$GITHUB_TOKEN
-fi
-
-if [ "$TRAVIS_REPO_SLUG" == "okfde/datenwirken.de" ]; then
-    TOK=$GITHUB_TOKEN_OKFDE
-fi
-if [[ ! -z $TOK ]]; then
-    git push -q --force "https://${TOK}@github.com/${TRAVIS_REPO_SLUG}.git" `git subtree split --prefix site master`:gh-pages
+if [[ ! -z $GITHUB_TOKEN]]; then
+    git push -q --force "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" `git subtree split --prefix site master`:gh-pages
 else
-    echo "Reposiory TOKEN could not was not set to any value"
+    echo "GITHUB_TOKEN variable not defined, we cannot push!"
     exit -1
 fi
