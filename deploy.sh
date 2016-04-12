2239ad619cb0917d5a87542135a6cc35838aaf43
Sebastian @ Travis CI <sebastian@zillessen.info>
2016-04-12 11:41:57 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-set -e # exit with nonzero exit code if anything fails
-# run our compile script, discussed above
-echo "Running build.py build"
-python build.py build
-
-echo "Adding all changes and commiting"
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
 git add .
-git commit -am "Automatically Deploy to GitHub Pages"
-git push "https://${GH_TOKEN}:x-oauth-basic@github.com/sebastianzillessen/datenwirken.de.git" master
-git push --force --quiet "https://${GH_TOKEN}:x-oauth-basic@github.com/sebastianzillessen/datenwirken.de.git" `git subtree split --prefix site master`:gh-pages > /dev/null 2>&1
+git commit -m "Deploy to Github Pages"
+echo "Token ${GITHUB_TOKEN}"
+echo "Repo ${GITHUB_REPO}"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" `git subtree split --prefix site master`:gh-pages > /dev/null 2>&1
