3f82dd83b658444398d6c04e387a58b47e30744f
Sebastian @ Travis CI <sebastian@zillessen.info>
2016-04-12 13:57:51 +0100

    changed order

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git config --global user.name "Travis CI"
 git add .
 git commit --amend --no-edit
 echo "We are on $TRAVIS_REPO_SLUG"
-if [[ ! -z $GITHUB_TOKEN]]; then
+if [[ ! -z $GITHUB_TOKEN ]]; then
     git push -q --force "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" `git subtree split --prefix site master`:gh-pages
 else
     echo "GITHUB_TOKEN variable not defined, we cannot push!"
