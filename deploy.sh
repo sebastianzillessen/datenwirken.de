1f523f05e73a508b87f965b84f31ecfed504a631
Sebastian @ Travis CI <sebastian@zillessen.info>
2016-04-12 20:45:50 +0100

    removed site from repro

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
+cd site
+git init
 git add .
-git commit --amend --no-edit
+git commit -am "Automatic build"
 echo "We are on $TRAVIS_REPO_SLUG"
 if [[ ! -z $GITHUB_TOKEN ]]; then
-    git push -q --force "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" `git subtree split --prefix site master`:gh-pages
+    git push -q --force "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
 else
     echo "GITHUB_TOKEN variable not defined, we cannot push!"
     exit -1
 fi
+cd ..
