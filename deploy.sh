bc305e61f399d847ad72107e5c722a516a9121d2
Sebastian @ Travis CI <sebastian@zillessen.info>
2016-04-12 13:47:15 +0100

    changed repository to TRAVIS_REPO_SLUG

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,18 @@ git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 git add .
 git commit -m "Deploy to Github Pages"
-REP=""
 TOK=""
 echo "We are on $TRAVIS_REPO_SLUG"
 if [ "$TRAVIS_REPO_SLUG" == "sebastianzillessen/datenwirken.de" ]; then
-    REP=$GITHUB_REPO
     TOK=$GITHUB_TOKEN
 fi
 
 if [ "$TRAVIS_REPO_SLUG" == "okfde/datenwirken.de" ]; then
-    REP=$GITHUB_REPO_OKFDE
     TOK=$GITHUB_TOKEN_OKFDE
 fi
-
-
-echo "Repository: '$REP'"
-if [[ ! -z $REP ]]; then
-    git push --force "https://${TOK}@github.com/${REP}.git" `git subtree split --prefix site master`:gh-pages
+if [[ ! -z $TOK ]]; then
+    git push -q --force "https://${TOK}@github.com/${TRAVIS_REPO_SLUG}.git" `git subtree split --prefix site master`:gh-pages
 else
-    echo "Reposiory was not set to any value"
+    echo "Reposiory TOKEN could not was not set to any value"
     exit -1
 fi
